7130d123a39ec6e994d17471c92b1342722f520c
Decker108 <1337decker@gmail.com>
2019-10-05 19:01:28 +0200

    WIP on animations

---
+++ Assets/Scripts/PhotoButton.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(GameObject))]
+public class PhotoButton : MonoBehaviour {
+    public GameObject zoomedPhotoGO;
+
+    void Start() {
+        
+    }
+
+    void Update() {
+        
+    }
+
+    public void handleOnClick() {
+        Debug.Log("Photo button clicked");
+        //enter zoomed photo mode
+        Vector3 photoPosition = new Vector3(0f, 0f, 0f);
+        GameObject newGo = Object.Instantiate(zoomedPhotoGO, photoPosition, Quaternion.identity);
+        Image i = (Image) newGo.GetComponent(typeof(Image));
+        // i.sprite = Sprite.Create()
+    }
+}
