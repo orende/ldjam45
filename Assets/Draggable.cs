59ef405322b5d3ab1b51289dd83fe81b1023f770
Johan Rende <johan@rende.se>
2019-10-05 13:25:38 +0200

    Ädded click

--- Assets/Draggable.cs
+++ Assets/Draggable.cs
@@ -5,105 +5,87 @@ using UnityEngine.EventSystems;
 
 
 public class Draggable : EventTrigger {
-    Canvas canvas;
     private bool isDragging = false;
+
     void Start() {
-        canvas = this.gameObject.GetComponent<Canvas>();             
     }
 
     void Update() {
      
     }
 
-    public void onClick() {
-        Debug.Log("hej");
-    }
     public override void OnBeginDrag(PointerEventData data)
     {
-        Debug.Log("OnBeginDrag called.");
+        isDragging = true;
     }
 
     public override void OnCancel(BaseEventData data)
     {
-        //Debug.Log("OnCancel called.");
     }
 
     public override void OnDeselect(BaseEventData data)
     {
-        //Debug.Log("OnDeselect called.");
     }
 
     public override void OnDrag(PointerEventData data)
     {
-//        canvas.
         transform.position += new Vector3(data.delta.x, data.delta.y, 0);
-        Debug.Log("position: " + transform.position);
-        Debug.Log("delta: " + data.delta);
-        Debug.Log("OnDrag called.");
     }
 
     public override void OnDrop(PointerEventData data)
     {
-        Debug.Log("OnDrop called.");
     }
 
     public override void OnEndDrag(PointerEventData data)
     {
-        Debug.Log("OnEndDrag called.");
+        isDragging = false;
     }
 
     public override void OnInitializePotentialDrag(PointerEventData data)
     {
-        Debug.Log("OnInitializePotentialDrag called.");
     }
 
     public override void OnMove(AxisEventData data)
     {
-        //Debug.Log("OnMove called.");
     }
 
     public override void OnPointerClick(PointerEventData data)
     {
-        Debug.Log("OnPointerClick called.");
+        if (!isDragging)
+        {
+            Debug.Log("Click.");
+        }
     }
 
     public override void OnPointerDown(PointerEventData data)
     {
-        Debug.Log("OnPointerDown called.");
     }
 
     public override void OnPointerEnter(PointerEventData data)
     {
-        Debug.Log("OnPointerEnter called.");
     }
 
     public override void OnPointerExit(PointerEventData data)
     {
-        //Debug.Log("OnPointerExit called.");
     }
 
     public override void OnPointerUp(PointerEventData data)
     {
-        Debug.Log("OnPointerUp called.");
     }
 
     public override void OnScroll(PointerEventData data)
     {
-        Debug.Log("OnScroll called.");
     }
 
     public override void OnSelect(BaseEventData data)
     {
-        Debug.Log("OnSelect called.");
     }
 
     public override void OnSubmit(BaseEventData data)
     {
-        Debug.Log("OnSubmit called.");
     }
 
     public override void OnUpdateSelected(BaseEventData data)
     {
-        //Debug.Log("OnUpdateSelected called.");
     }
 }
