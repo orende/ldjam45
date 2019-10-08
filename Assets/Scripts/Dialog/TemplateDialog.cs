876c7d73f62b639cecea4f5e13c81284e71c85d1
Joel Björkland <joel.bjorkland@groplay.com>
2019-10-08 09:12:15 +0200

    Added a End to the game

---
+++ Assets/Scripts/Dialog/TemplateDialog.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TemplateDialog : DialogHandler
+{
+    TemplateDialog()
+    {
+        this.entries.Add(
+            new DialogEntry("NPC",
+            "The Dialog"));
+    }
+}
---
+++ Assets/Scripts/Dialog/TheEndDialog.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TheEndDialog : DialogHandler
+{
+    TheEndDialog()
+    {
+        this.entries.Add(
+            new DialogEntry("MrM",
+            "Hi. Who are you?"));
+        this.entries.Add(
+            new DialogEntry("detective",
+            "I am a detective. Your boss wanted me to find you."));
+        this.entries.Add(
+            new DialogEntry("MrM",
+            "Well, pleas tell him that I still have a couple of weeks left on my vacation."));
+        this.entries.Add(
+            new DialogEntry("detective",
+            "..."));
+        this.entries.Add(
+            new DialogEntry("detective",
+            "Sure"));
+        this.entries.Add(
+            new DialogEntry("detective",
+            "Have a nice vacation..."));
+        this.entries.Add(
+            new DialogEntry("MrM",
+            "Thank you!"));
+    }
+}
