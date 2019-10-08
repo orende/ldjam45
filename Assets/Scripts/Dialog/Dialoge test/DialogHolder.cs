dd520cc23cb91b72bbff55a236e69b9baf23f74b
Joel Björkland <joel.bjorkland@groplay.com>
2019-10-08 21:19:44 +0200

    scriptebul objekt test

---
+++ Assets/Scripts/Dialog/Dialoge test/DialogHolder.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Dialog",menuName = "Dialog")]
+public class DialogHolder : ScriptableObject
+{
+    public TextAsset dialog;
+    public Sprite character;
+}
